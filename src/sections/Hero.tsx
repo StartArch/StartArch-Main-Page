1588fdae4c746bcff6c209e3ec8d344bd69e4f3e
Vinicius Souza <me@viniciusals.com>
2023-01-02 13:14:02 -0500

    Fix html indentation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,8 +10,7 @@ function Hero() {
                 <h1>Propel your business <br/>to a new height <br/>with a modern website</h1>
 
                 <h2>
-                    We are a team of enthusiastic engineers, designers and developers <br className='hidden md:inline'/>
-                    with the experience and know-how to bring your projects into reality
+                    We are a team of enthusiastic engineers, designers and developers <br className='hidden md:inline'/>with the experience and know-how to bring your projects into reality
                 </h2>
             </div>
 

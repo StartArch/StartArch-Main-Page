b783574b9d19acf27ee2ea04176cc9d27a6bd783
Vinicius Souza <me@viniciusals.com>
2023-01-01 19:11:23 -0500

    Adds images to project's assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,15 @@
 import startarchLogo from './startarchLogo.svg';
 import yellowRocket from './yellow-rocket.png';
+import designIcon from './design-icon.svg';
+import figmaLogo from './figma-logo.svg';
+import adobeXDLogo from './adobeXD-logo.svg';
+import photoshopLogo from './photoshop-logo.svg';
 
 export {
     startarchLogo,
-    yellowRocket
+    yellowRocket,
+    designIcon,
+    figmaLogo,
+    adobeXDLogo,
+    photoshopLogo
 }

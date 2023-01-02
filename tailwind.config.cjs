c5abfca04bd63dda88e071fb8368669ae2cfde9c
Vinicius Souza <me@viniciusals.com>
2023-01-01 23:26:53 -0500

    Sets tailwind themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+        extend: {
+            colors: {
+                nearWhite: '#F3F3F3',
+                nearBlack: '#333333'
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                dmSans: ['DM Sans', 'sans-serif'],
+                spaceGrotesk: ['Space Grotesk', 'sans-serif']
+            }
+        },
+        screens: {
+            xs: '480px',
+            ss: '620px',
+            sm: '768px',
+            md: '1060px',
+            lg: '1400px',
+            xl: '1700px'
+        }
   },
   plugins: [],
 }

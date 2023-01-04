7c299d975397313805f5417e2db9fcdd1ad7e125
Vinicius Souza <me@viniciusals.com>
2023-01-04 03:30:23 -0500

    Updates services section to use serviceCard component

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,4 +1,5 @@
 import styles from './Services.module.css';
+import { ServiceCard } from '../components';
 import { designIcon, figmaLogo, adobeXDLogo, photoshopLogo } from '../assets';
 import styles from '../styles/services-section.module.css';
 
@@ -20,25 +21,10 @@ function Services() {
                     <li className={servicesListItems(false)}>Development</li>
                     <li className={servicesListItems(false)}>Hosting & Maintenance</li>
                 </ul>
-                <div className={styles.serviceDescription}>
-
-                    <img src={designIcon} alt='Design' width='60px'/>
-                    <p>
-                        Every project starts at the drawing table, we work to bring what is in your head to reality.
-                    </p>
-                    <ul className={styles.serviceDistinctions}>
-                        <li>Website design</li>
-                        <li>Mobile app design</li>
-                        <li>Logo design</li>
-                    </ul>
-                    <div className={styles.serviceTools}>
-                        <img src={figmaLogo} alt='Figma'/>
-                        <img src={adobeXDLogo} alt='AdobeXD'/>
-                        <img src={photoshopLogo} alt='Photoshop'/>
+                {/* <ServiceCard projectInfo={designProjectInfo}/> */}
+                {/* <ServiceCard projectInfo={developmentProjectInfo}/> */}
+                <ServiceCard projectInfo={hostingProjectInfo}/>
                     </div>
-                </div>
-            </div>
-
         </section>
     );
 }

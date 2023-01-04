86576f1862eeeb98ae05870505120b2fb1da2c78
Vinicius Souza <me@viniciusals.com>
2023-01-04 03:27:44 -0500

    Adds services icons to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,19 @@
+import adobeXDLogo from './adobeXD-logo.svg';
+import designIcon from './design-icon.png';
+import developmentIcon from './development-icon.png';
+import figmaLogo from './figma-logo.svg';
+import hostingIcon from './hosting-icon.png';
+import photoshopLogo from './photoshop-logo.svg';
 import startarchLogo from './startarchLogo.svg';
 import yellowRocket from './yellow-rocket.webp';
-import designIcon from './design-icon.svg';
-import figmaLogo from './figma-logo.svg';
-import adobeXDLogo from './adobeXD-logo.svg';
-import photoshopLogo from './photoshop-logo.svg';
 
 export {
-    startarchLogo,
-    yellowRocket,
-    designIcon,
-    figmaLogo,
     adobeXDLogo,
-    photoshopLogo
+    designIcon,
+    developmentIcon,
+    figmaLogo,
+    hostingIcon,
+    photoshopLogo,
+    startarchLogo,
+    yellowRocket
 }

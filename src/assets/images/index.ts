d68d62acd40f0691f5a9d24a4a9123e00117795b
Vinicius Souza <me@viniciusals.com>
2023-01-05 02:33:14 -0500

    Creates objects holding image and descriptions

---
+++ src/assets/images/index.ts
@@ -0,0 +1,62 @@
+import adobeXDLogo from './adobeXD-logo.svg';
+import designIcon from './design-icon.png';
+import developmentIcon from './development-icon.png';
+import figmaLogo from './figma-logo.svg';
+import hostingIcon from './hosting-icon.png';
+import photoshopLogo from './photoshop-logo.svg';
+import startarchLogo from './startarchLogo.svg';
+import yellowRocket from './yellow-rocket.webp';
+
+import {ImportedStaticImages} from '../../types';
+
+
+const AdobeXDLogo: ImportedStaticImages = {
+    image: adobeXDLogo,
+    description: "AdobeXD"
+}
+
+const DesignIcon: ImportedStaticImages = {
+    image: designIcon,
+    description: "Design services"
+}
+
+const DevelopmentIcon: ImportedStaticImages = {
+    image: developmentIcon,
+    description: "Development services"
+}
+
+const FigmaLogo: ImportedStaticImages = {
+    image: figmaLogo,
+    description: "Figma"
+}
+
+const HostingIcon: ImportedStaticImages = {
+    image: hostingIcon,
+    description: "Hosting services"
+}
+
+const PhotoshopLogo: ImportedStaticImages = {
+    image: photoshopLogo,
+    description: "Photoshop"
+}
+
+const StartarchLogo: ImportedStaticImages = {
+    image: startarchLogo,
+    description: "Startarch logo"
+}
+
+const YellowRocket: ImportedStaticImages = {
+    image: yellowRocket,
+    description: "Yellow rocket"
+}
+
+export {
+    AdobeXDLogo,
+    DesignIcon,
+    DevelopmentIcon,
+    FigmaLogo,
+    HostingIcon,
+    PhotoshopLogo,
+    StartarchLogo,
+    YellowRocket
+}

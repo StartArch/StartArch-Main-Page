c4e2a09b45d8a61c06ef2512133648b8a74c50d1
Vinicius Souza <me@viniciusals.com>
2023-01-08 22:08:29 -0500

    Removes attempt to make navbar responsive

--- src/core/navbar/Navbar.tsx
+++ src/core/navbar/Navbar.tsx
@@ -5,48 +5,16 @@ import styles from './navbar.module.css';
 
 function Navbar() {
 
-    let [isMobile, setIsMobile] = useState(false);
-    let [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => setIsMenuOpen(prev => !prev);
-
-    const checksWindowWidth = () => {
-        if (window.innerWidth > 1060) {
-            setIsMenuOpen(false);
-            setIsMobile(false);
-        }
-        else {
-            setIsMobile(true);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener("resize", checksWindowWidth);
-        return () =>
-            window.removeEventListener("resize", checksWindowWidth);
-    });
-
-    const menuIcon = isMenuOpen ? CloseMenu.asset : HamburguerMenu.asset;
-    const menuIconDescription = isMenuOpen ? CloseMenu.description : HamburguerMenu.description;
-
-
-    const navbarStyle = isMenuOpen ? styles.navSection + ' bg-darkPurple h-[100vh]' : styles.navSection;
-
-    const navButtons = (isMobile && !isMenuOpen) ? 'hidden ' + styles.navButtons : styles.navButtons;
-
     return (
-        <nav className={navbarStyle}>
+        <nav className={styles.navSection}>
 
             <span className={styles.companyLogo}>
                 <img src={StartarchLogo.asset} alt={StartarchLogo.description} />
                 <h1>StartArch</h1>
             </span>
 
-            <img className={styles.hamburguerMenu}
-                onClick={toggleMenu} src={menuIcon} alt={menuIconDescription}/>
-
             {/* TODO: Settup react router to navigate through page */}
-            <div className={navButtons}>
+            <div className={styles.navButtons}>
                 <button className={styles.button}>About</button>
                 <button className={styles.button}>Portfolio</button>
                 <button className={styles.button}>Services</button>

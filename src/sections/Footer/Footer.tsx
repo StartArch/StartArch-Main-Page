2b9a30cd7aec2a28c1ce31729ce52826c9967f23
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2023-02-08 23:43:49 -0300

    i got it to define the size, just left 3 tabs

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,10 +1,22 @@
 import styles from './Footer.module.css';
+import { StartarchLogoNotTitle } from '../../assets/images';
 
 
 function Footer() {
     return (
         <div className={styles.footer}>
-            <h2 className={styles.titulo}>StartArch</h2>
+            <div className={styles.columnOganize}>
+                <img className="h-[100px] w-auto"
+                    src={StartarchLogoNotTitle.asset} alt={StartarchLogoNotTitle.description} />
+                <h2 className={styles.titulo}>StartArch</h2>
+                <h4 className={styles.slogan}>
+                We help brands turn their products into reality. 
+                </h4>
+                <h2 className={styles.email}>
+                hello@startarch.com
+                </h2>
+            </div>
+
         </div>
     );
 }

c5b22f73b3bc78bc5e2f1631add34ad1d1743f9b
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2023-02-09 23:40:50 -0300

    final style

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -10,11 +10,39 @@ function Footer() {
                     src={StartarchLogoNotTitle.asset} alt={StartarchLogoNotTitle.description} />
                 <h2 className={styles.titulo}>StartArch</h2>
                 <h4 className={styles.slogan}>
-                We help brands turn their products into reality. 
+                    We help brands turn their products into reality.
                 </h4>
                 <h2 className={styles.email}>
-                hello@startarch.com
+                    hello@startarch.com
                 </h2>
+
+                <div className={styles.tags}>
+
+                    <div className={styles.columnTags}>
+                        <div className={styles.firstLabel}>Company</div>
+                        <div className={styles.otherLabel}>Projects</div>
+                        <div className={styles.otherLabel}>Services</div>
+                        <div className={styles.otherLabel}>Prices</div>
+                        <div className={styles.otherLabel}>Contact</div>
+
+                    </div>
+
+                    <div className={styles.columnTags}>
+                        <div className={styles.firstLabel}>Support</div>
+                        <div className={styles.otherLabel}>Login</div>
+                        <div className={styles.otherLabel}>Create an account</div>
+                        <div className={styles.otherLabel}>Privacy policy</div>
+                        <div className={styles.otherLabel}>Terms and conditions</div>
+                    </div>
+
+                    <div className={styles.columnTags}>
+                    <div className={styles.firstLabel}>social medias</div>
+                        <div className={styles.otherLabel}>Linkedin</div>
+                        <div className={styles.otherLabel}>Instagram</div>
+                        <div className={styles.otherLabel}>Twitter</div>
+                        <div className={styles.otherLabel}>GitHub</div>
+                    </div>
+                </div>
             </div>
 
         </div>

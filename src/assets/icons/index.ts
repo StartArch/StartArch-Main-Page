c85a5221189c91f2670c7d4d6f069723a64a96f2
Vinicius Souza <me@viniciusals.com>
2023-02-19 08:36:40 -0500

    Adds new icons to assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,13 @@
 import chatIcon from './chatIcon.svg';
 import closeMenu from './closeMenu.svg';
+import database from './database.svg';
 import devIcon from './devIcon.svg';
 import flexibilityIcon from './flexibilityIcon.svg';
 import hamburguerMenu from './hamburguerMenu.svg';
 import moneyIcon from './moneyIcon.svg';
+import monitor from './monitor.svg';
+import phone from './phone.svg';
+import puzzlePieces from './puzzlePieces.svg';
 
 export const ChatIcon: ImportedStaticAsset = {
     asset: chatIcon,
@@ -13,6 +17,11 @@ export const ChatIcon: ImportedStaticAsset = {
 export const CloseMenu: ImportedStaticAsset = {
     asset: closeMenu,
     description: "Close menu button"
+}
+
+export const Database: ImportedStaticAsset = {
+    asset: database,
+    description: "Database icon"
 }
 
 export const DevIcon: ImportedStaticAsset = {
@@ -33,4 +42,19 @@ export const HamburguerMenu: ImportedStaticAsset = {
 export const MoneyIcon: ImportedStaticAsset = {
     asset: moneyIcon,
     description: "Coin icon"
+}
+
+export const Monitor: ImportedStaticAsset = {
+    asset: monitor,
+    description: "Monitor icon"
+}
+
+export const Phone: ImportedStaticAsset = {
+    asset: phone,
+    description: "Phone icon"
+}
+
+export const PuzzlePieces: ImportedStaticAsset = {
+    asset: puzzlePieces,
+    description: "Integrations icon"
 }

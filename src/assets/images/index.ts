2e73285639285fdfc4c96cc13d4834903cb197ef
Vinicius Souza <me@viniciusals.com>
2023-02-19 08:43:35 -0500

    Removes unused assets

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,42 +1,5 @@
-import adobeXDLogo from './adobeXD-logo.svg';
-import designIcon from './design-icon.png';
-import developmentIcon from './development-icon.png';
-import figmaLogo from './figma-logo.svg';
-import hostingIcon from './hosting-icon.png';
-import photoshopLogo from './photoshop-logo.svg';
 import startarchLogo from './startarchLogo.svg';
 import yellowRocket from './yellow-rocket.webp';
-import startarchLogoNotTitle from './startarchLogoNotTitle.svg';
-
-export const AdobeXDLogo: ImportedStaticAsset = {
-    asset: adobeXDLogo,
-    description: "AdobeXD"
-}
-
-export const DesignIcon: ImportedStaticAsset = {
-    asset: designIcon,
-    description: "Design services"
-}
-
-export const DevelopmentIcon: ImportedStaticAsset = {
-    asset: developmentIcon,
-    description: "Development services"
-}
-
-export const FigmaLogo: ImportedStaticAsset = {
-    asset: figmaLogo,
-    description: "Figma"
-}
-
-export const HostingIcon: ImportedStaticAsset = {
-    asset: hostingIcon,
-    description: "Hosting services"
-}
-
-export const PhotoshopLogo: ImportedStaticAsset = {
-    asset: photoshopLogo,
-    description: "Photoshop"
-}
 
 export const StartarchLogo: ImportedStaticAsset = {
     asset: startarchLogo,
@@ -46,10 +9,4 @@ export const StartarchLogo: ImportedStaticAsset = {
 export const YellowRocket: ImportedStaticAsset = {
     asset: yellowRocket,
     description: "Yellow rocket"
-}
-
-
-export const StartarchLogoNotTitle: ImportedStaticAsset = {
-    asset: startarchLogoNotTitle,
-    description: "Startarch logo"
 }

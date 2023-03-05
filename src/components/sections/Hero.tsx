441c2225fcd5676ee07f20ab1c16c970057a574c
Vinicius Souza <me@viniciusals.com>
2023-03-05 01:47:37 -0500

    Updates hero title and refactors styles

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
     return (
         <section className={styles.heroSection}>
             <div className={styles.textBox}>
-                <h1>Propel your business <br/>to a new height <br/>with a modern website</h1>
+                <h1>Your ideas <br/>start here</h1>
 
                 <h2>
                     We are a team of enthusiastic engineers, designers and developers <br className='hidden md:inline'/>with the experience and know-how to bring your projects into reality

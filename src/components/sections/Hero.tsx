716fb7a9d70847bb5e89dcb386a50828a99dc1c4
Vinicius Souza <me@viniciusals.com>
2023-03-11 14:08:30 -0500

    Updates hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { YellowRocket } from '../../assets/images';
+import { StartarchLogo } from '../../assets/images';
 import styles from '../../styles/Hero.module.css';
 
 function Hero() {
@@ -10,9 +10,15 @@ function Hero() {
                 <h1>Your ideas <br/>start here</h1>
 
                 <h2>
-                    We are a team of enthusiastic engineers, designers and developers <br className='hidden md:inline'/>with the experience and know-how to bring your projects into reality
+                    We are a team of enthusiastic engineers, designers and developers with the experience and know-how to bring your projects into reality
                 </h2>
             </div>
+
+            <Image
+                className={styles.companyLogo}
+                src={StartarchLogo.asset}
+                alt={StartarchLogo.description}/>
+
         </section>
     );
 }
